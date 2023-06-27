892d76bd63235413b66205d43897a6d71316ed5d
sumitflyweis <node3@flyweis.technology>
2023-06-27 12:06:03 +0530

    f

--- index.js
+++ index.js
@@ -33,35 +33,6 @@ mongoose
 
 
 
-
-
-
-/// restaurant routes require
-
-
-
-
-/*
-   Require Routes End
-*/
-
-
-
-/*
-    Use Routes
-*/
-//  const userCreateRoutes = require('./route/user/user.create');
-//  const getRestaurantByUser = require('./route/user/getRestaurantCreate')
-//  const bookingRoutes = require('./route/user/booking')
-//  const paymentRoute = require('./route/user/payment')
-//  const coupenRoute = require('./route/user/coupen')
-//  const cancellationPolicyRoute = require('./route/user/cancellationPolicy')
-//  const bookingCateringServicesRoute = require('./route/user/bookingOfCateringServices')
-//  const paymentOfCateringServiceRoute = require('./route/user/paymentOfCateringServices')
-//  const notificationUserRoute = require ('./route/user/notification')
-//  const reviewRoute = require('./route/user/review')
-
-
 /// user routes use
 app.use('/api', require('./route/user/user.create'));
 app.use('/api',require('./route/user/getRestaurantCreate'))
@@ -73,28 +44,6 @@ app.use('/api',require('./route/user/bookingOfCateringServices'))
 app.use('/api',require('./route/user/paymentOfCateringServices'))
 app.use('/api',require ('./route/user/notification'))
 app.use('/api',require('./route/user/review'))
-
-
-
-
-/// admin routes require
-// const adminAuthRoutes = require('./route/admin/adminCreate');
-// const paymentAdminRoute = require('./route/admin/payment')
-// const coupenAdminRoute = require('./route/admin/coupen')
-// const cancellationAdminRoute = require('./route/admin/cancellationPolicy')
-// const booking = require('./route/admin/booking')
-// const bookingCateringServicesAdminRoute = require('./route/admin/bookingOfCateringServices')
-// const paymentOfCateringServiceAdminRoute = require('./route/admin/paymentOfCateringServices')
-// const notificationAdminRoute = require('./route/admin/notification')
-// const restaurantCreate = require("./route/admin/restaurantCreate")
-// const userCreate = require("./route/admin/userCreate")
-// const deliveryAgents = require ('./route/admin/deliveryAgents')
-// const privacy = require('./route/admin/privacy')
-// const aboutus  = require('./route/admin/aboutus')
-//const subscription = require('./route/admin/subscription')
-//const reviewAdminRoute = require('./route/admin/review')
-//const bookingAdminRoutes = require('./route/admin/booking')
-// const adminPlanTypeRoute = require('./routes/admin/PlanType.js/PlanType')
 
 
 
@@ -115,22 +64,6 @@ app.use('/api',require('./route/admin/aboutus'))
 //app.use('/api',subscription)
 //app.use('/api',reviewAdminRoute)
 //app.use('/api',bookingAdminRoutes)
-
-
-// const restaurantRoutes = require('./route/restaurant/restaurantCreate');
-// const restaurantBannerRoutes = require('./route/restaurant/restaurantBanner');
-// const planTypeRoutes =  require('./route/restaurant/restaurantPlanType')
-// const mealRoute = require('./route/restaurant/restaurantMeal')
-// const dishesRoute = require('./route/restaurant/dishes')
-// const paymentRestaurantRoute  = require('./route/restaurant/payment')
-// const coupenRestaurantRoute = require('./route/restaurant/coupen')
-// const cancellationRestaurantRoute = require('./route/restaurant/cancellationPolicy')
-// const bookingCateringServicesRestaurantRoute = require('./route/restaurant/bookingOfCateringServices')
-// const paymentOfCateringServicesRestaurantRoute = require('./route/restaurant/paymentOfCateringServices')
-// const notificationRestaurantRoute = require('./route/restaurant/notification')
-// //const reviewRestaurantRoute = require("./route/restaurant/review")
-// const bookingRestaurantRoutes = require('./route/restaurant/booking')
-
 
 
 // /// restaurant routes use
